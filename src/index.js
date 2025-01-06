3628a5b8d594a1c6c17e7fb1328f3afb887b3cb2
Subeen Park <psb0304022@gmail.com>
2025-01-07 02:22:05 +0900

    cicd test-1

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ app.get('/', (req,res)=>{
     res.send('Hello World!')
 });
 
+app.get('/hello',(req,res)=> {
+    res.send("hi");
+})
 app.listen(port,()=>{
     console.log(`Example app listening on port ${port}`);
 });
